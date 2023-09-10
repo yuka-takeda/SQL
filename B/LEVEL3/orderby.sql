c5ccde28c32502c1e51091b434eafa96f2451a51
2304060 <>
2023-09-10 22:23:15 +0900

    #10【題材B LEVEL3（第4章）】商店データベース

---
+++ B/LEVEL3/orderby.sql
@@ -0,0 +1,57 @@
+/※25
+SELECT 商品コード,商品名
+FROM 商品
+WHERE 商品区分 = '1'
+ORDER BY 商品コード DESC
+/※26
+SELECT 注文日,注文番号,注文枝番,商品コード,数量
+FROM 注文
+WHERE 注文日 >= '2018-03-31'
+ORDER BY 商品コード
+/※27
+SELECT DISTINCT商品コード
+FROM 注文
+ORDER BY 商品コード
+/※28
+SELECT 注文日
+FROM 注文
+ORDER BY 注文日 DESC
+OFFSET 0 ROWS
+FETCH NEXT 10 ROWS ONLY
+/※29
+SELECT 注文日
+FROM 注文
+ORDER BY 注文日 DESC
+OFFSET 0 ROWS
+FETCH NEXT 10 ROWS ONLY
+/※30
+SELECT *
+FROM 商品
+ORDER BY 単価
+OFFSET 5 ROWS
+FETCH NEXT 15 ROWS ONLY
+WHERE 単価 = 単価
+ORDER BY 商品区分,商品コード
+/※31
+SELECT 商品コード
+FROM 商品
+EXCEPT
+SELECT 商品コード
+FROM 注文
+ORDER BY 商品コード
+/※32
+SELECT 商品コード
+FROM 商品
+INTERSECT
+SELECT 商品コード
+FROM 注文
+ORDER BY 商品コード DESC
+/※33
+SELECT 商品コード,商品名,単価
+FROM 商品
+WHERE 商品区分 = '9'
+AND 単価 <= 1000
+AND 単価 >= 10000
+ORDER BY 単価
+WHERE 単価 = 単価
+ORDER BY 商品コード
